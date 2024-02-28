818935bbe49ef0d785afc8dc4ad552f8e3c33914
ashen8810 <ashen8810@gmail.com>
2024-02-28 16:45:59 +0530

    new feature added

--- app.py
+++ app.py
@@ -4,18 +4,14 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Endpoint for receiving time series data via POST request
 @app.route('/timeseries', methods=['POST'])
 def handle_timeseries():
-    # Check if the request contains JSON data
     if request.is_json:
         try:
             data = request.get_json()  # Get JSON data from the request
-            # Assuming the JSON data contains a list of time series data points
             time_series_data = data.get('time_series', [])
 
             date = '2023-06-01'
-            # time_series_data = "2023-06-05"
             
             timestamp1 = pd.Timestamp(date)
             timestamp2 = pd.Timestamp(time_series_data)
@@ -24,24 +20,15 @@ def handle_timeseries():
             
             predictions = loaded_model.get_prediction(start=timestamp1, end=timestamp2,dynamic=True,step=30)
 
-            # Extract the predicted values
             predicted_values = predictions.predicted_mean
             pred = predicted_values
 
-
-
-            
-            # Here you can perform any processing or analysis on the time series data
-            # For example, you could pass it to your SARIMAX model for prediction
-            
-            # Dummy response indicating successful processing
             response = {
                 'status': 'success',
-                'message': pred.to_json(orient='index')
+                'message': pred.to_json(orient='records')
             }
             return jsonify(response), 200
         except Exception as e:
-            # Handle any exceptions that occur during processing
             error_response = {
                 'status': 'error',
                 'message': 'An error occurred while processing the time series data',
@@ -53,11 +40,9 @@ def handle_timeseries():
         return jsonify({'error': 'Request must contain JSON data'}), 400
 @app.route('/timeserieswithexog', methods=['POST'])
 def handle_timeseries_with_exog():
-    # Check if the request contains JSON data
     if request.is_json:
         try:
             data = request.get_json()  # Get JSON data from the request
-            # Assuming the JSON data contains a list of time series data points
             time_series_data = data.get('date')
             exog_rainfall = data.get('rainfall')
             exog_holiday = data.get('holiday')
@@ -67,7 +52,6 @@ def handle_timeseries_with_exog():
             date = '2023-06-01'
 
             dates = pd.date_range(start=date, periods=len(exog_rainfall)) 
-            # exog_test = pd.DataFrame([exog_rainfall, exog_holiday],columns=["Rainfall","Holiday"],index=dates)
             exog_test = pd.DataFrame({"Rainfall": exog_rainfall, "Holiday": exog_holiday}, index=dates)
 
 
@@ -82,7 +66,7 @@ def handle_timeseries_with_exog():
             pred = predicted_values
             response = {
                 'status': 'success',
-                'message': pred.to_json(orient='index')
+                'message': pred.to_json(orient='records')
             }
             return jsonify(response), 200
         except Exception as e:
@@ -94,7 +78,6 @@ def handle_timeseries_with_exog():
             }
             return jsonify(error_response), 500
     else:
-        # Return error response if the request does not contain JSON data
         return jsonify({'error': 'Request must contain JSON data'}), 400
 
 if __name__ == '__main__':
